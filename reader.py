5cb688c7909c338b5129bb3e361e09ee118298d0
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2019-01-09 15:52:14 -0600

    Initial Commit

---
+++ reader.py
@@ -0,0 +1,19 @@
+import re
+
+# IMPORT pytesseract, allowing use of 'OTC Tesseract' image-to-text processor
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+# IMPORT system-specific variables
+import config
+
+
+# PATH to OTC Tesseract ['..\Tesseract-OCR\tesseract']
+pytesseract.pytesseract.tesseract_cmd = config.tesseract_install_path
+
+if __name__ == "__main__":
+    img_str = pytesseract.image_to_string(Image.open('src/img/salmon.jpg'))
+    print(img_str)
